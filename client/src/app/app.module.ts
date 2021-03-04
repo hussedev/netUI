ebbc9db5286f5828ab0a8327a1fedee3e779da23
huss <hussein.odeh@gmail.com>
2021-03-04 09:44:48 +0100

    feat: client: generated components and service

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { DeviceListComponent } from './device-list/device-list.component';
+import { DeviceSingleComponent } from './device-single/device-single.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavBarComponent,
+    DeviceListComponent,
+    DeviceSingleComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/device-single/device-single.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-single',
+  templateUrl: './device-single.component.html',
+  styleUrls: ['./device-single.component.css']
+})
+export class DeviceSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
